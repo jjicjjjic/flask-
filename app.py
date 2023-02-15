8122278dda9fc04346d1b4042f6092c59093cba1
jihui <jihui8653@daum.net>
2023-02-15 17:24:08 +0900

    added mysql db

--- app.py
+++ app.py
@@ -2,27 +2,29 @@ from flask import Flask, session, render_template, request, redirect, url_for
 from flaskext.mysql import MySQL
 import os
 
+
 mysql = MySQL()
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = '0000'
-app.config['MYSQL_DATABASE_DB'] = 'user_info'
+app.config['MYSQL_DATABASE_DB'] = 'music'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.secret_key='ABCDEFG'
+
 mysql.init_app(app)
+
 
 @app.route('/', methods=['GET','POST'])
 def main():
     error = None
 
     if request.method == 'POST':
-        id = request.form['inputEmail']
-        pw = request.form['inputPassword']
+        id = request.form['id']
+        pw = request.form['pw']
 
-        conn = mysql.connet()
+        conn = mysql.connect()
         cursor = conn.cursor()
-        sql = "SELECT email FROM users WHERE email = %s AND pw = %s"
+        sql = "SELECT id FROM users WHERE id = %s AND pw = %s"
         value = (id, pw)
         cursor.execute("set names utf8")
         cursor.execute(sql, value)
@@ -35,19 +37,21 @@ def main():
             data = row[0]
 
         if data:
-            session['login_user'] = id()
+            session['login_user'] = id
             return redirect(url_for('home'))
         else :
             error='invalid input data detected !'
-        return render_template('LoginPage.html', error = error)
+    return render_template('LoginPage.html', error = error)
  
  
 @app.route('/RegisterPage.html', methods=['GET', 'POST'])
 def register():
     error = None
     if request.method == 'POST':
-        id = request.form['inputEmail']
-        pw = request.form['inputPassword']
+        id = request.form['regi_id']
+        print('id:', id)
+        pw = request.form['regi_pw']
+        print('pw:', pw)
  
         conn = mysql.connect()
         cursor = conn.cursor()
@@ -71,8 +75,50 @@ def register():
 @app.route('/MainPage.html', methods=['GET', 'POST'])
 def home():
     error = None
-    email = session['login_user']
-    return render_template('MainPage.html', error=error, name=id)
+    id = session['login_user']
+
+    if request.method == 'POST':
+        content = request.form['content']
+        conn = mysql.connect()
+        
+        cursor = conn.cursor()
+        sql = "INSERT INTO contents (id, content, times) VALUES ('%s', '%s', now())" %(id, content)
+        cursor.execute(sql)
+        new_data = cursor.fetchall()
+
+        if not new_data:
+            conn.commit()
+            return redirect(url_for("home"))
+        else:
+            conn.rollback()
+            return "Register Failed"
+        
+        cursor.close()
+        conn.close()
+
+    elif request.method == "GET":
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        sql = " SELECT content, id, times, num FROM contents ORDER BY num desc"
+        cursor.execute(sql)
+        data = cursor.fetchall()
+
+        data_list = []
+
+        for obj in data:
+            data_dict = {
+                'con':obj[0],
+                'writer':obj[1],
+                'time':obj[2],
+                'num':obj[3]
+            }
+
+            data_list.append(data_dict)
+        cursor.close()
+        conn.close()
+
+        return render_template('MainPage.html', error = error, name = id, data_list=data_list)
+    return render_template('MainPage.html', error=error, name= id)
  
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8000)), debug=True, use_reloader=False)
